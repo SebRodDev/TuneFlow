e7c95e08129795d711716804af8e90272fb6550b
Sebastian Rodriguez <sebrod35@purdue.edu>
2025-03-20 21:33:44 -0400

    progress

---
+++ javascript/signIn.js
@@ -0,0 +1,8 @@
+const button = document.getElementById('soundCloudButton');
+const clientId = '576b749d69f04b67b12a836f942f605c';
+const redirectURI = 'http://127.0.0.1:5500/music';
+
+button.addEventListener('click', function() {
+
+
+});
